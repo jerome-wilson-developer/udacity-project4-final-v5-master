ccf14f232881162c23de6601aba21cb61da1a7cd
jerome-wilson-developer <48839642+jerome-wilson-developer@users.noreply.github.com>
2022-08-31 17:59:19 -0400

    push old folders to a new repo ... repo v5.

---
+++ README.md
@@ -0,0 +1,48 @@
+
+## Grow-with-Google Project #4: "Landing Page Project" from my Google-Udacity Scholarship NanoDegree Bootcamp for Front End Developers.
+ 
+## My Netlify link for the Project #4 is:  
+https://udacity-project4-landing-page-v4main.netlify.app/  
+
+## This is a 1-page Landing Page with 5 sections: 
+    * The challenges required to build this Landing Page adds to the user having a great User Experience while using this app. 
+    * This project included a challenging task to virtually create the navigation elements in the DOM instead of hardcoding the list items and anchor tags in index.html. 
+    * Other challenges were to highlight the active section on the page, along with the corresponding active list item, so that the user always knows exactly which section they are reading. 
+    * Also, whenever the user clicks a list item, this project required code that allowed the viewport to scroll smoothly from section to section.  
+
+## This Front End Developer Nanodegree (FEND) Bootcamp:
+This bootcamp is a coding scholarship program financed by Google to increase the number of high-level, skilled developers around the world.
+
+### The starter code for this project:
+- The starter code for this project has some HTML and CSS styling to display a static version of the Landing Page project. 
+- I am required to convert this project from a static project to an interactive one. 
+- That requires modifying the files to make this application interactive and dynamic.
+
+### The files used for this project are:
+    * HTML
+    * CSS
+    * but the majority of added files are JavaScript.
+    * the Udacity starter zip file comes with no javascript starter files.
+
+### The functionalities required to meet this project's specifications are:  
+    * Write clean, simple, modular code that is easy to read by others.
+    * Write enough comments for the code to make it easy to understand the logic.
+    * Highlight the Active section so the visitor knows which section they are on. 
+    * Highlight the Active nav links so the visitor knows which link they are on.
+    * I used 'Smooth Scroll - scrollIntoView' method to improve the User Experience.
+    * Make the viewport responsive on all screen sizes ... added media queries for phone and tablet.
+    * Create li and anchor links dynamically using the dom and javascript.
+    * Improve the User Interface to make the Landing Page easier to navigate and to consume the content.
+    * Improve the User Experience to make learning enjoyable, friendly and comfortable when learning new knowledge.
+
+### The Research for this project included:    
+    * Guidance from my weekly Session Leader - Rockwell, different Slack Leaders, and suggestions from the project reviewers    
+    * Google browser's Inspect Elements and Dev Page Source tools
+    * YouTube walkthroughs, freeCodeCamp, etc
+    * YouTube tutorials such as Kevin Powell, Brad Traversy, Cifer, etc.
+    * StackOverflow questions and answers
+    * w3schools interactive tutorials
+    * MDN Web documentation
+    * Other tutorials found on different search engines.
+# udacity-project4-final-v5-master
+# udacity-project4-final-v5-master
---
+++ js/app.js
@@ -0,0 +1,103 @@
+
+/***use jquery's .ready method to load all assets before tne page loads.*/
+$(document).ready(function() {
+
+/***Define Global Variables.*/
+/*grab the list items.*/
+const list_items = document.querySelector('#navbar__list');
+/*target each section in the html body*/
+const sections = document.querySelectorAll('section');
+
+/***create a function that dynamically displays the nav's list items & links in the dom.*/
+// Begin Main Functions
+const createListElements = function() {
+    //declare an empty string variable to be filled dynamically via js.
+    let navbarElements = '';
+
+    //run a forEach loop thru each section to target the list items and links.
+    sections.forEach(section => {
+
+    //run forEach loop to target the sectionID in each section.
+    const sections = section.id;
+    
+    //grab the data_nav in each section.
+    const data = section.getAttribute('data-nav');
+    
+    //js dynamically adds the list items and anchors to the dom elements.
+    navbarElements += `<li id="li-${sections}"> 
+    <a class="menu__link" href="#${sections}"> ${data}
+    </a></li>`;
+  });   
+    //add and print the list items and anchors to the dom.
+    list_items.innerHTML = navbarElements;
+}
+//call the anonymous function 
+createListElements();
+
+
+
+/***declare a function that removes the active class from sections and list items that are not active.*/
+function removeClass(section) {  
+
+  //grab the section.id attributes from all non-active sections.
+  const isActive = section.getAttribute('id');
+  
+  //add li to section. remove all non-active sections and list items.
+  document.getElementById('li-' + isActive).classList.remove('your-active-class');
+  
+  //remove the active background of non-active sections and list items.
+  section.style.backgroundColor = 'transparent';
+}
+/***Build the code to add an Active Class.*/
+//declare a function that grabs the sectionID and then adds active when at the Top of the viewport.
+function addClass(section) { 
+
+  //grab the section.id attributes from all sections.
+  const isActive = section.getAttribute('id');
+  
+  //add the active li to the active section.
+  document.getElementById('li-' + isActive).classList.add('your-active-class');
+
+  //highlight backgrounds with a different color.
+  section.style.backgroundColor = 'lightcoral';
+}
+/***declare a function to determine top and bottom position of current active section.*/
+ function activeClass() {  
+    //write a foreach loop to determine top and bottom coordinates of the active section.
+    sections.forEach((section) => {  
+      let elementOffset = section.getBoundingClientRect();
+
+      //declare the if/else conditions to add or remove the active section based on the elementOffset top and bottom coordinates.
+      if (elementOffset.top <= 150 && elementOffset.bottom >= 150) {
+        addClass(section);
+      } else {
+        removeClass(section);
+    }
+  });
+}    
+//event handler
+document.addEventListener('scroll', activeClass);
+
+
+
+/***create the main function to use scrollIntoview method for smooth scroll.*/
+const smoothScroll = () => {
+
+    //loop thru the menu anchors and add a click event to the element. 
+    document.querySelectorAll('.navbar__menu li .menu__link').forEach((anchor) => {
+      anchor.addEventListener("click", function (e) {
+
+        //prevent the default response.
+        e.preventDefault();
+        //grab links and implement the scrollIntoView method using behavior:'smooth'.
+        document.querySelector(anchor.getAttribute("href")).scrollIntoView({
+          behavior: "smooth",
+        });
+      });
+    });
+  }
+
+// Scroll to the section clicked by the link.
+smoothScroll();
+
+}); 
